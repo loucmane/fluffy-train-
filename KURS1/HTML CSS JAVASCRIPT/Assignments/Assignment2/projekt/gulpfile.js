d3a0aa7f314705b97dd5c05269edbe951eeb0533
Loucmane <lookmanbenali@gmail.com>
2020-10-08 17:31:48 +0200

    lets try doing this assignment stuff my guy

---
+++ KURS1/HTML CSS JAVASCRIPT/Assignments/Assignment2/projekt/gulpfile.js
@@ -0,0 +1,13 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let csso = require('gulp-csso');
+
+
+gulp.task('default', function(){
+    
+    return gulp.src('scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'))
+    .pipe(csso())
+    .pipe(gulp.dest('dist'));
+});
