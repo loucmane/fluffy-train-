325644f3b1b60adb995387d81ed1fa5ba55e954c
Loucmane <lookmanbenali@gmail.com>
2020-11-09 12:15:28 +0100

    stuck on making new lists, it keeps the old inputs and adds new ones onto it

--- KURS2/JAVASCRIPT GRUNDKURS/assignments/assignment1/projectTemplate/js/main.js
+++ KURS2/JAVASCRIPT GRUNDKURS/assignments/assignment1/projectTemplate/js/main.js
@@ -1,21 +1,38 @@
 
 window.onload = () => {
     
-    let tasksContainer =  document.querySelector('[data-lists]')
+    const tasksContainer =  document.querySelector('[data-tasks__lists]')
+    const tasksNewListForm = document.querySelector('[data-tasks__new-lists-form]')
+    const tasksNewListInput = document.querySelector('[data-tasks__new-lists-input]')
     
-    
-    
-    let tasksLists = [{
-        id: 1
-        name: 'name'
-    }, 'todo']  
+    let tasksLists = []  
+
+
+    tasksNewListForm.addEventListener('submit', e => {
+        e.preventDefault()
+        const tasksListName = tasksNewListInput.value
+        if (tasksListName == null || tasksListName === '' ) return
+        const tasksList = createTasksList(tasksListName)
+        tasksNewListInput.value = null
+        tasksLists.push(tasksList)
+        render()
+    })
+
+
+    function createTasksList(name) {
+      return {id: Date.now().toString(), name: name, tasks: []}
+    }
+
+
+
 
     function render() {
         clearElement(tasksContainer)
         tasksLists.forEach(tasksList => {
-            const tasksListElement = document.createElement('li');
+            const tasksListElement = document.createElement('li'); //creates a list item
+            tasksListElement.dataset.tasksListID = tasksList.id //sets the id on lists item
             tasksListElement.classList.add("tasks__list-item");
-            tasksListElement.innerText = tasksList;
+            tasksListElement.innerText = tasksList.name;
             tasksContainer.appendChild(tasksListElement)
 
         })
