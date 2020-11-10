62b25a7a3e5c9bf43b0778b27a7722009814442f
Loucmane <lookmanbenali@gmail.com>
2020-11-10 14:07:39 +0100

    fixed issue with multiple inputs, clearElement function didnt work. fixed with .innerhTML = '';

--- KURS2/JAVASCRIPT GRUNDKURS/assignments/assignment1/projectTemplate/js/main.js
+++ KURS2/JAVASCRIPT GRUNDKURS/assignments/assignment1/projectTemplate/js/main.js
@@ -1,13 +1,18 @@
 
 
     
-    const tasksContainer =  document.querySelector('[data-tasks__lists]');
+    const taskListContainer =  document.querySelector('[data-tasks__lists]');
     const tasksNewListForm = document.querySelector('[data-tasks__new-lists-form]');
     const tasksNewListInput = document.querySelector('[data-tasks__new-lists-input]');
     
     const  LOCAL_STORAGE_TASKS_LIST_KEY = 'tasks.list';
     let tasksLists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TASKS_LIST_KEY)) || [];
 
+
+
+     //make sure you have a task list that updates or pushes one time
+
+     //
 
     tasksNewListForm.addEventListener('submit', e => {
         e.preventDefault();
@@ -36,22 +41,23 @@
 
 
     function render() {
-        clearElement(tasksContainer);
+        // clearElement(taskListContainer);
+         taskListContainer.innerHTML = '';
         tasksLists.forEach(tasksList => {
             const tasksListElement = document.createElement('li'); //creates a list item
             tasksListElement.dataset.tasksListID = tasksList.id; //sets the id on lists item
             tasksListElement.classList.add("tasks__list-item");
             tasksListElement.innerText = tasksList.name;
-            tasksContainer.appendChild(tasksListElement);
+            taskListContainer.appendChild(tasksListElement);
 
         });
     }
 
-    function clearElement(element) {
-        while (element.firstchild) {
-            element.removeChild(element.firstchild);
-        }
-    }
+    // function clearElement(element) { cant get this clearElement part to work for some reason.
+    //     while (element.firstchild) { 
+    //       element.firstChild.remove();
+    //     }
+    // }
 
     render();
 
